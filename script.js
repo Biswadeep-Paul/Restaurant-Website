e6037691f1d3b01adeedcc8e1c676f7d32b700be
Biswadeep-Paul <160915483+Biswadeep-Paul@users.noreply.github.com>
2024-03-19 20:32:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let contact_submit= document.getElementById("contact_submit");
+contact_submit.addEventListener("click",function(){
+    alert("Submitted successfully");
+});
+
+
+
+let submit = document.getElementById("reservation_submit");
+reservation_submit.addEventListener("click",function(){
+    alert("Submitted Successfully");
+});
